051dc464b2ce75137ab11024511569366b3ef510
Sebastian Bohorquez <sebastianbohorquez4607@gmail.com>
2026-05-19 11:47:54 -0500

    Refactor comments and structure in HelloWorld.js

--- hello-javascript-main/Basic/00-HelloWord.js
+++ hello-javascript-main/Basic/00-HelloWord.js
@@ -1,1 +1,15 @@
-console.log("¡Hola Mundo!")
+
+
+// Este es un comentario de una sola línea
+
+/*
+ESTE ES 
+UN COMENTARIO DE 
+VARIAS LINEAS
+ */
+
+// Ejemplos de como imprimir texto en pantalla:
+
+console.log("¡Hola Mundo!")  
+console.log('Holab Sebastian')
+
